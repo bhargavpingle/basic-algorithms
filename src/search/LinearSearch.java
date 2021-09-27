51318978fb49dd42cfee5cffff8037be80f68813
bhargav <bhargavreddypingle@gmail.com>
2021-09-27 15:02:00 -0500

    adding comment for linear search

--- src/search/LinearSearch.java
+++ src/search/LinearSearch.java
@@ -3,8 +3,10 @@ package search;
 public class LinearSearch {
     public static int search(int[] arr, int target) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) return i;
+            if (arr[i] == target) {
+                return i; // Return the index if target is found
+            }
         }
-        return -1;
+        return -1; // Return -1 if target is not found
     }
 }

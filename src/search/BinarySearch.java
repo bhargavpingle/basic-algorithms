ee8c09c152a4372e63f4ce853353adb70105f108
bhargav <bhargavreddypingle@gmail.com>
2021-10-27 15:02:00 -0500

    adding comment for Binary  search

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -2,13 +2,23 @@ package search;
 
 public class BinarySearch {
     public static int search(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
+        int left = 0;
+        int right = arr.length - 1;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (arr[mid] == target) return mid;
-            if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
+
+            if (arr[mid] == target) {
+                return mid; // Target found at mid
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1; // Discard the left half
+            } else {
+                right = mid - 1; // Discard the right half
+            }
         }
-        return -1;
+
+        return -1; // Return -1 if target is not found
     }
 }

f619339b4aebb55dd902e3ba3d6163330944dabf
bhargav <bhargavreddypingle@gmail.com>
2021-11-08 15:02:00 -0600

    Recursive Binary  search logic added

--- src/search/RecursiveBinarySearch.java
+++ src/search/RecursiveBinarySearch.java
@@ -2,27 +2,27 @@ package search;
 
 public class RecursiveBinarySearch {
 
-    // Existing linearSearch method here...
+    // Existing methods here...
 
     /**
-     * Performs iterative binary search on a sorted array.
+     * Performs recursive binary search on a sorted array.
      * @param arr Sorted array to search.
      * @param target The target element to find.
+     * @param left Left boundary index.
+     * @param right Right boundary index.
      * @return Index of the target if found, otherwise -1.
      */
-    public static int binarySearchIterative(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            }
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    public static int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        if (left > right) return -1;
+
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
         }
-        return -1;
+        if (arr[mid] < target) {
+            return binarySearchRecursive(arr, target, mid + 1, right);
+        } else {
+            return binarySearchRecursive(arr, target, left, mid - 1);
+        }
     }
 }

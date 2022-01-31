b5adb57f9d36235cfb48d54931e07e0ec27aff53
bhargav <bhargavreddypingle@gmail.com>
2022-01-31 15:02:00 -0600

    quick sort logic added

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,4 +1,36 @@
 package sort;
 
 public class QuickSort {
+
+    public static void sort(int[] arr) {
+        sortHelper(arr, 0, arr.length -1);
+    }
+
+    private static void sortHelper(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            sortHelper(arr, low, pi-1);
+            sortHelper(arr, pi+1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low -1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
 }

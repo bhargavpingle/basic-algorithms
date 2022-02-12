5c3e4dd17805a9e09526967b357a05179c2d831d
bhargav <bhargavreddypingle@gmail.com>
2022-02-12 15:02:00 -0600

    bubble sort enhancement logic added

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -2,20 +2,63 @@ package sort;
 
 public class BubbleSort {
 
-    public static void sort(int[] arr) {
+    /**
+     * Enhanced Bubble Sort implementation with pass and swap counters.
+     * Also supports verbose mode to show array state per pass.
+     *
+     * @param arr    the array to be sorted
+     * @param verbose if true, prints array after each pass
+     */
+    public static void sort(int[] arr, boolean verbose) {
+        if (arr == null || arr.length <= 1) {
+            System.out.println("No sorting needed (null or single-element array).");
+            return;
+        }
+
         int n = arr.length;
-        boolean swapped;
+        int totalSwaps = 0;
+        int pass = 0;
+
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
+            boolean swapped = false;
+            pass++;
+
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                     swapped = true;
+                    totalSwaps++;
                 }
             }
+
+            if (verbose) {
+                System.out.printf("Pass %d: ", pass);
+                printArray(arr);
+            }
+
+            // If no elements were swapped in a full pass, the array is sorted
             if (!swapped) break;
         }
+
+        System.out.printf("Bubble Sort completed in %d pass(es) with %d swap(s).\n", pass, totalSwaps);
+    }
+
+    /**
+     * Overloaded method with verbose=false by default.
+     */
+    public static void sort(int[] arr) {
+        sort(arr, false);
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
     }
 }

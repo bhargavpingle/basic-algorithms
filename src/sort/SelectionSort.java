bf8c8164695d9c3ebe6742501881fb794fc59139
bhargav <bhargavreddypingle@gmail.com>
2022-03-12 15:02:00 -0600

    selection sort enhancement logic added

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -2,18 +2,64 @@ package sort;
 
 public class SelectionSort {
 
-    public static void sort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n -1; i++) {
+    /**
+     * Enhanced Selection Sort with comparison/swap count and optional verbose output.
+     *
+     * @param arr     the array to sort
+     * @param verbose if true, prints array state after each outer iteration
+     */
+    public static void sort(int[] arr, boolean verbose) {
+        if (arr == null || arr.length <= 1) {
+            System.out.println("No sorting needed (null or single-element array).");
+            return;
+        }
+
+        int comparisons = 0;
+        int swaps = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
             int minIdx = i;
-            for (int j = i +1; j < n; j++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+                comparisons++;
                 if (arr[j] < arr[minIdx]) {
                     minIdx = j;
                 }
             }
-            int temp = arr[minIdx];
-            arr[minIdx] = arr[i];
-            arr[i] = temp;
+
+            if (minIdx != i) {
+                swap(arr, i, minIdx);
+                swaps++;
+            }
+
+            if (verbose) {
+                System.out.printf("Step %d: ", i + 1);
+                printArray(arr);
+            }
         }
+
+        System.out.printf("Selection Sort completed with %d comparison(s) and %d swap(s).\n", comparisons, swaps);
+    }
+
+    /**
+     * Overloaded method for default usage (non-verbose).
+     */
+    public static void sort(int[] arr) {
+        sort(arr, false);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
     }
 }

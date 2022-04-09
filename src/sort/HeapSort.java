c4f901d6b00430612e3ec2bf0c93f47b44e20fc8
bhargav <bhargavreddypingle@gmail.com>
2022-04-09 15:02:00 -0500

    heap sorting logic added

--- src/sort/HeapSort.java
+++ src/sort/HeapSort.java
@@ -1,4 +1,45 @@
 package sort;
 
 public class HeapSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract elements
+        for (int i = n - 1; i >= 0; i--) {
+            swap(arr, 0, i); // Move current root to end
+            heapify(arr, i, 0); // Heapify reduced heap
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Initialize root as largest
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        }
+    }
 }

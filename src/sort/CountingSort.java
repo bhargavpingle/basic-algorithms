ac9ebe630ae08d5825438864c3479b2c1c0dddb8
bhargav <bhargavreddypingle@gmail.com>
2022-04-22 15:02:00 -0500

    counting sorting logic added

--- src/sort/CountingSort.java
+++ src/sort/CountingSort.java
@@ -1,4 +1,26 @@
 package sort;
 
+import java.util.Arrays;
+
 public class CountingSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        int max = Arrays.stream(arr).max().orElse(0);
+        int min = Arrays.stream(arr).min().orElse(0);
+
+        int range = max - min + 1;
+        int[] count = new int[range];
+        int[] output = new int[arr.length];
+
+        for (int num : arr) count[num - min]++;
+        for (int i = 1; i < count.length; i++) count[i] += count[i - 1];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            output[count[arr[i] - min] - 1] = arr[i];
+            count[arr[i] - min]--;
+        }
+
+        System.arraycopy(output, 0, arr, 0, arr.length);
+    }
 }

c65ad44e6ff74df1d2af43b301ddf0b1749a7e21
bhargav <bhargavreddypingle@gmail.com>
2022-05-22 15:02:00 -0500

    shell sort logic added

--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -1,4 +1,17 @@
 package sort;
 
 public class ShellSort {
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                    arr[j] = arr[j - gap];
+                arr[j] = temp;
+            }
+        }
+    }
 }

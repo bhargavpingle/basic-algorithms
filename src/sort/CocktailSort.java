1651a5225ba68c2c1677ccdf47545d849efeeb55
bhargav <bhargavreddypingle@gmail.com>
2022-08-02 15:02:00 -0500

    Cocktails sort logic added

--- src/sort/CocktailSort.java
+++ src/sort/CocktailSort.java
@@ -1,4 +1,38 @@
 package sort;
 
 public class CocktailSort {
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        boolean swapped = true;
+        int start = 0, end = arr.length - 1;
+
+        while (swapped) {
+            swapped = false;
+
+            for (int i = start; i < end; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+
+            swapped = false;
+            end--;
+
+            for (int i = end - 1; i >= start; i--) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            start++;
+        }
+    }
 }

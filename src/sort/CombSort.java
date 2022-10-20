253864d5b3313625b470e9231a9a889fcde2f303
bhargav <bhargavreddypingle@gmail.com>
2022-10-20 15:02:00 -0500

    comb sort logic added

--- src/sort/CombSort.java
+++ src/sort/CombSort.java
@@ -1,4 +1,29 @@
 package sort;
 
 public class CombSort {
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        int gap = n;
+        boolean swapped = true;
+
+        while (gap != 1 || swapped) {
+            gap = getNextGap(gap);
+            swapped = false;
+
+            for (int i = 0; i < n - gap; i++) {
+                if (arr[i] > arr[i + gap]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + gap];
+                    arr[i + gap] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    private static int getNextGap(int gap) {
+        gap = (gap * 10) / 13;
+        return (gap < 1) ? 1 : gap;
+    }
 }

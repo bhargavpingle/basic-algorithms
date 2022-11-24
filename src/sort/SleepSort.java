8411fff37e2fd34bd2186e3f0cdd272142673f9a
bhargav <bhargavreddypingle@gmail.com>
2022-11-24 15:02:00 -0600

    Sleep sort logic added

--- src/sort/SleepSort.java
+++ src/sort/SleepSort.java
@@ -1,4 +1,26 @@
 package sort;
 
+import java.util.concurrent.CountDownLatch;
+
 public class SleepSort {
+
+    public static void sort(int[] arr) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(arr.length);
+
+        for (int num : arr) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(num * 10L); // 10ms per unit
+                    System.out.print(num + " ");
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                } finally {
+                    latch.countDown();
+                }
+            }).start();
+        }
+
+        latch.await(); // Wait for all threads to finish
+        System.out.println();
+    }
 }

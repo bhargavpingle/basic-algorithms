5b3f8d5dd6076f7cb05e7a7cc035c364bd274268
bhargav <bhargavreddypingle@gmail.com>
2022-11-30 15:02:00 -0600

    pigeon hole sort logic added

--- src/sort/PigeonholeSort.java
+++ src/sort/PigeonholeSort.java
@@ -1,4 +1,24 @@
 package sort;
 
+import java.util.*;
+
 public class PigeonholeSort {
+
+    public static void sort(int[] arr) {
+        if (arr == null || arr.length <= 1) return;
+
+        int min = Arrays.stream(arr).min().orElse(0);
+        int max = Arrays.stream(arr).max().orElse(0);
+        int range = max - min + 1;
+
+        List<List<Integer>> holes = new ArrayList<>(range);
+        for (int i = 0; i < range; i++) holes.add(new ArrayList<>());
+
+        for (int num : arr) holes.get(num - min).add(num);
+
+        int index = 0;
+        for (List<Integer> hole : holes)
+            for (int num : hole)
+                arr[index++] = num;
+    }
 }

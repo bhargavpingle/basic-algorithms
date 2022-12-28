d8e496dd5659ec2d6a1d4ba25ae613564e6198be
bhargav <bhargavreddypingle@gmail.com>
2022-12-28 15:02:00 -0600

    OddEven sort logic added

--- src/sort/OddEvenSort.java
+++ src/sort/OddEvenSort.java
@@ -1,4 +1,31 @@
 package sort;
 
 public class OddEvenSort {
+
+    public static void sort(int[] arr) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 1; i < arr.length - 1; i += 2) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    sorted = false;
+                }
+            }
+
+            for (int i = 0; i < arr.length - 1; i += 2) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    sorted = false;
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }

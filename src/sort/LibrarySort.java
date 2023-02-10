fc31be2256134408674f3ce984a6ae7d65e6b357
bhargav <bhargavreddypingle@gmail.com>
2023-02-10 15:02:00 -0600

    library sort logic added

--- src/sort/LibrarySort.java
+++ src/sort/LibrarySort.java
@@ -1,4 +1,18 @@
 package sort;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LibrarySort {
+
+    public static void sort(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+            int pos = Collections.binarySearch(list, num);
+            list.add(pos < 0 ? -pos - 1 : pos, num);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = list.get(i);
+        }
+    }
 }

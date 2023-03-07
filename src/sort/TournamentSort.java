d2203239ff6e7cfa66925ccf67c420844b4f9e6f
bhargav <bhargavreddypingle@gmail.com>
2023-03-07 15:02:00 -0600

    tournament sort logic added

--- src/sort/TournamentSort.java
+++ src/sort/TournamentSort.java
@@ -1,4 +1,12 @@
 package sort;
 
+import java.util.PriorityQueue;
+
 public class TournamentSort {
+
+    public static void sort(int[] arr) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num : arr) heap.offer(num);
+        for (int i = 0; i < arr.length; i++) arr[i] = heap.poll();
+    }
 }

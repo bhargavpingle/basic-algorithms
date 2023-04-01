d1779aa4e4d06540a7dda21303e87e9438ec89ed
bhargav <bhargavreddypingle@gmail.com>
2023-04-01 15:02:00 -0500

    spaghetti sort logic added

--- src/sort/SpaghettiSort.java
+++ src/sort/SpaghettiSort.java
@@ -1,4 +1,18 @@
 package sort;
 
+import java.util.TreeMap;
+
 public class SpaghettiSort {
+
+    public static void sort(int[] arr) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : arr) map.put(num, map.getOrDefault(num, 0) + 1);
+
+        int index = 0;
+        for (var entry : map.entrySet()) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                arr[index++] = entry.getKey();
+            }
+        }
+    }
 }

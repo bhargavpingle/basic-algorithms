61cb18885bea539480bf2c35f15fc930302403eb
bhargav <bhargavreddypingle@gmail.com>
2023-05-20 14:02:00 -0500

    stooge sort logic added

--- src/sort/StoogeSort.java
+++ src/sort/StoogeSort.java
@@ -1,4 +1,19 @@
 package sort;
 
 public class StoogeSort {
+
+    public static void sort(int[] arr, int l, int h) {
+        if (arr[l] > arr[h]) {
+            int temp = arr[l];
+            arr[l] = arr[h];
+            arr[h] = temp;
+        }
+
+        if (h - l + 1 > 2) {
+            int t = (h - l + 1) / 3;
+            sort(arr, l, h - t);
+            sort(arr, l + t, h);
+            sort(arr, l, h - t);
+        }
+    }
 }

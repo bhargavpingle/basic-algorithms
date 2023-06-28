2f2d2b5130cda39909cf5adce4d5bc8e056335b0
bhargav <bhargavreddypingle@gmail.com>
2023-06-28 14:02:00 -0500

    bitwise sort logic added

--- src/sort/BitwiseSort.java
+++ src/sort/BitwiseSort.java
@@ -1,4 +1,29 @@
 package sort;
 
 public class BitwiseSort {
+    public static void sort(int[] arr) {
+        sort(arr, 0, arr.length, 31); // 31 bits for signed int
+    }
+
+    private static void sort(int[] arr, int left, int right, int bit) {
+        if (bit < 0 || right - left <= 1)
+            return;
+
+        int i = left, j = right - 1;
+        while (i <= j) {
+            while (i <= j && ((arr[i] >> bit) & 1) == 0)
+                i++;
+            while (i <= j && ((arr[j] >> bit) & 1) == 1)
+                j--;
+
+            if (i < j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        sort(arr, left, i, bit - 1);
+        sort(arr, i, right, bit - 1);
+    }
 }

d53f32e48c6fea1f4731a169a2d4ccc6ff287b58
bhargav <bhargavreddypingle@gmail.com>
2023-07-29 14:02:00 -0500

    brick sort logic added

--- src/sort/BrickSort.java
+++ src/sort/BrickSort.java
@@ -1,4 +1,21 @@
 package sort;
 
 public class BrickSort {
+    public static void sort(int[] arr) {
+        int min = arr[0], max = arr[0];
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        int range = max - min + 1;
+        int[] holes = new int[range];
+
+        for (int num : arr) holes[num - min]++;
+        int index = 0;
+
+        for (int i = 0; i < range; i++) {
+            while (holes[i]-- > 0) arr[index++] = i + min;
+        }
+    }
 }
